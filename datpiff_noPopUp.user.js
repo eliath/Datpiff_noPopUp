b3d7deedb8cd339a770634c405e78d150b1fa803
eliath <eliasmartinezcohen@gmail.com>
2015-03-01 14:04:56 -0500

    changed filename and attempt to override openMixtape function (does not work)

---
+++ datpiff_noPopUp.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name			Datpiff - no pop up
+// @description		This script opens the datpiff mixtape player launched by pressing the "listen" button in a same window rather than a new popup.
+// @namespace		http://eliath.biz/
+// @match			http://www.datpiff.com/*-mixtape.*.html*
+// @run-at			document-end
+//
+// @version			2.0
+// 
+// ==/UserScript==
+
+console.log("Datpiff mixtape detected");
+unsafeWindow.openMixtape = function(media_id, track) {
+	console.log("* openMixtape(...) OVERRIDE *");
+	track = (track) ? track : 1;
+	var url = 'http://www.datpiff.com/pop-mixtape-player-2014.2.php?mediaid='+ media_id + '&trackid=' + track;
+	unsafeWindow.open(url, "_self");
+};
